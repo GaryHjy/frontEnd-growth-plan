2a3d1b2151dcc3ea7a201c97e9bef18533dd31c6
GaryHjy <782316959@qq.com>
2020-10-07 01:07:58 +0800

    feat: 修改siteConfig

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,8 +23,8 @@ const users = [
 const siteConfig = {
   title: '前端成长计划', // Title for your website.
   tagline: 'frontEnd growth plan',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://GaryHjy.github.io', // Your website URL
+  baseUrl: '/frontEnd-growth-plan', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
